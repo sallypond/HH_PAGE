e14cbc57f08ec5b2ab6419ee771194e333b8fb59
sallypond <sally@pondstudio.tv>
2024-07-17 17:46:25 +0900

    20240717

--- js/languagedata.js
+++ js/languagedata.js
@@ -5,16 +5,18 @@ const data = {
             "about-sub": "시너지의 폭발",
             "about-text":
                 "Hyper Hole는 게임 제작의 중심에서 최고의 창의력을 끌어내는 스튜디오입니다. 다양한 기술이 조합되어 빅뱅을 일으키듯, Hyper Hole은 팀워크를 통해 놀라운 게임을 탄생시킵니다. 우리는 우주의 퀘이사처럼 빛나는 게임을 만들기 위해, 초거대 블랙홀처럼 강력한 협업을 추구합니다. Hyper Hole에서 모든 팀원이 하나 되어 최고의 게임을 창조합니다.",
-            "maintext": "Hyper Hole의 로고는 다양한 기술과 팀워크의 시너지가 모여 최고의 게임을 만들기 위한 우리의 무한한 에너지를 상징합니다. 퀘이사의 강렬한 폭발처럼, 우리의 게임과 로고는 창의력과 조화의 결과로 탄생한 빛나는 경험을 제공합니다. ",
+            "maintext": "퀘이사는 격렬히 폭발하지만 모든것을 빨아들이는 블랙홀이 그 폭발력의 원천입니다. Hyper Hole의 로고는 퀘이사의 에너지가 뿜어지는 모습과 안을 향해 빨아들여지는 질량을 형상화하여 다양한 기술의 시너지, 조합이 폭발하는 모습을 표현하였습니다.",
             "gametext": "Hyper Hole는 게임 제작의 중심에서 최고의 창의력을 끌어내는 스튜디오입니다. 다양한 기술이 조합되어 빅뱅을 일으키듯, Hyper Hole은 팀워크를 통해 놀라운 게임을 탄생시킵니다. ",
+            "phone": "phone: 031-332-9990",
         },
         "en": {
             "moldtitle": "Mold Farm",
             "about-sub": "explosion of synergy",
             "about-text":
                 "Hyper Hole is a studio that brings out the best creativity at the heart of game development. Just as various substances combine to create the Big Bang, Hyper Hole brings together diverse individuals to create amazing games. To create games that shine like quasars in the universe, we pursue powerful collaboration, like a supermassive black hole. At Hyper Hole, every team member unites to create the best games.",
-            "maintext": "The Hyper Hole logo symbolizes the explosive synergy of diverse skills and teamwork, much like the radiant burst of a quasar. Just as a quasar explodes with energy, our games offer brilliant experiences born from creativity and collaboration. The logo represents our boundless energy and our pursuit of creating the best games.",
+            "maintext": "Quasars erupt violently, but the source of this explosive power is a black hole that absorbs everything. The logo of Hyper Hole symbolizes the energy emitted by a quasar and the mass being drawn inward, expressing the explosive synergy and combination of various technologies.",
             "gametext": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magn",
+            "phone": "phone: +82); 31-332-9990",
         },
     },
 
